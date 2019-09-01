c678b86bb0fa4b893dc7c04a83642e7f6488dea9
zhangyuhang64 <zhangyuhang64@gmail.com>
2019-09-01 11:01:59 -0400

    first commit

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace TreeSort
+{
+    public class Node
+    {
+        public int item;
+        public Node left;
+        public Node right;
+        public void Display()
+        {
+            Console.Write("[" + item + "]");
+        }
+    }
+    public class Tree
+    {
+        public Node root;
+        public Tree()
+        {
+            root = null;
+        }
+        public Node ReturnRoot()
+        {
+            return root;
+        }
+        public void Insert(int id)
+        {
+            Node newNode = new Node();
+            newNode.item = id;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (id < current.item)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        public void Preorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Console.Write(Root.item + " ");
+                Preorder(Root.left);
+                Preorder(Root.right);
+            }
+        }
+        public void Inorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Inorder(Root.left);
+                Console.Write(Root.item + " ");
+                Inorder(Root.right);
+            }
+        }
+        public void Postorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Postorder(Root.left);
+                Postorder(Root.right);
+                Console.Write(Root.item + " ");
+            }
+        }
+    }
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        [STAThread]
+        static void Main()
+        {
+            Tree theTree = new Tree();
+            theTree.Insert(50);
+            theTree.Insert(17);
+            theTree.Insert(23);
+            theTree.Insert(12);
+            theTree.Insert(19);
+            theTree.Insert(54);
+            theTree.Insert(9);
+            theTree.Insert(14);
+            theTree.Insert(67);
+            theTree.Insert(76);
+            theTree.Insert(72);
+            Console.WriteLine("Inorder Traversal : ");
+            theTree.Inorder(theTree.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("Preorder Traversal : ");
+            theTree.Preorder(theTree.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("Postorder Traversal : ");
+            theTree.Postorder(theTree.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+
+        }
+    }
+}
